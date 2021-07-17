0dc1579eff304caa7ee589f2e6c9247be2f95c64
Emanuelvss13 <61998824+Emanuelvss13@users.noreply.github.com>
2021-07-17 14:37:50 -0300

    update branch

--- Aula 3 & 4 & 5 - Matheus Silva/src/graphql/typeDefs.js
+++
@@ -1,7 +0,0 @@
-import { fileLoader, mergeTypes } from "merge-graphql-schemas";
-import path from "path";
-
-const typesArray = fileLoader(path.join(__dirname, 'module', '**', '*.gql'))
-const typesDefs = mergeTypes(typesArray)
-
-export default typesDefs
